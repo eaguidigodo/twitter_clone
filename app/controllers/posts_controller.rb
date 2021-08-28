f4aa08fb9b05a9d312291e2b6b9f035e4200089d
eaguidigodo <eaguidigodo@gmail.com>
2021-08-28 03:35:29 +0100

    Edit function not working properly

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,14 @@ class PostsController < ApplicationController
     puts "Je suis ici"#{params[:post][:content]}
     redirect_to posts_index_path
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post.update(params[:id])
+    redirect_to posts_index_path, notice: "Post successfully updated!"
+  end
+
 end
