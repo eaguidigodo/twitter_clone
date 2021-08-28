997a93cd9a8dd71bbf38c9dd3ff2b25332d7666b
eaguidigodo <eaguidigodo@gmail.com>
2021-08-28 01:48:43 +0100

    Implemented the create functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def create 
+    Post.create(content: params[:post][:content])
+    puts "Je suis ici"#{params[:post][:content]}
+    redirect_to posts_index_path
+  end
 end
